99ffeb03df344d2dbc3096f0c046531476da7469
etombert <you@example.com>
2017-01-23 15:23:10 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!-- DEBUT NAV DESKTOP -->
+<nav class="desktop">
+	HEADER
+</nav>
+<!-- FIN NAV DESKTOP -->
+
+<!-- DEBUT NAV RESPONSIVE -->
+<nav class="mobile">
+	
+</nav>
+<!-- FIN NAV RESPONSIVE -->
---
+++ pages/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>		
+		<title>Welcome</title>
+		<?php include '../head.php';?>
+	</head>
+	<body id="" class="">
+		<header>
+			<?php include '../header.php';?>
+		</header>
+
+		<main class="main">
+			MAIN
+		</main>
+
+		<footer>
+			<?php include '../footer.php';?>
+		</footer>
+	</body>
+</html>
